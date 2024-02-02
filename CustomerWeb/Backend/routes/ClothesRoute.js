c788390b7a38e427e58d81a90267a8c6f85263d7
ariyaltharun <ariyaltharun@gmail.com>
2024-02-02 21:12:15 +0000

    Add Primitive search engine and Autocompletion functionality
    [ DETAILS ]
    * Display the products that match query with the product doc (mongodb
      doc)
    * Give autocompletion while searching the products.
    
    [ UNDER THE HOOD ]
    * Create search index in mongodb atlas
    	- One Search Index for full text search
    	- Other search Index for autocomplete
    * full-text search: Search all the fields in the document
    * Using mongodb aggregation to filter the products based on
      search query

--- CustomerWeb/Backend/routes/ClothesRoute.js
+++ CustomerWeb/Backend/routes/ClothesRoute.js
@@ -5,13 +5,64 @@ const router = Router()
 
 router.get("/getClothes", async (req, res) => {
     const Item = await Items.find({}).limit(10)
-    // console.log(req.cookies.jwt);
-    // console.log(Object.keys(Item[0]))
-    // console.log(Item[0].Rate);
-    // console.log(req.cookies.jwt);
     res.status(200).json(Item)
-    // res.send([{}]);
 })
 
+/* Search AutoComplete Feature */
+router.get("/autocomplete/:query", async (req, res) => {
+    console.log("Autocomplete route");
+    const agg = [
+        {
+            $search:
+            {
+                index: "autocomplete",
+                autocomplete: {
+                    query: req.params.query,
+                    path: "ItemName"
+                }
+            }
+        },
+        {
+            $project: {
+                ItemName: 1,
+            }
+        },
+        { 
+            $limit: 10 
+        },
+    ];
+    const items = await Items.aggregate(agg);
+    res.status(200).json(items)
+})
+
+/* Search items based on query */
+router.get("/search/:query", async (req, res) => {
+    console.log("Search route");
+    let query = req.params.query;
+    let agg = [
+        {
+            $search:
+            {
+                index: "ItemsSearch",
+                text: {
+                    query: query,
+                    path: {
+                        wildcard: "*"
+                    }
+                }
+            }
+        }, 
+        {
+            $project: {
+                embedding: 0
+            }
+        },
+        { 
+            $limit: 10 
+        }
+    ]
+    const items = await Items.aggregate(agg).limit(10);
+    res.send(items);
+})
 
 export default router
--- CustomerWeb/Frontend/components/Shop.jsx
+++ CustomerWeb/Frontend/components/Shop.jsx
@@ -10,6 +10,7 @@ export const Shop = () => {
   const [clothes, setClothes] = useState([])
   const [prev, setPrev] = useState(0)
   const [next, setNext] = useState(10)
+  const [autocomplete, setAutocomplete] = useState([])
 
   currentPage(1)
 
@@ -20,6 +21,38 @@ export const Shop = () => {
         console.log(clothes);
       })
   }, [])
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    let query = e.target.value;
+
+    let autocompleteList = await fetch(`http://localhost:3000/autocomplete/${query}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include'
+    }).then((data) => data.json())
+
+    setAutocomplete(autocompleteList);
+    console.log(autocomplete);
+  }
+
+  const fetchItems = async (e) => {
+    // e.preventDefault();
+    if (e.key === 'Enter') {
+      let query = e.target.value;
+      let res = await fetch(`http://localhost:3000/search/${query}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        credentials: 'include'
+      }).then((data) => data.json())
+      console.log(res);
+      setClothes(res)
+    }
+  }
 
   const addToCart = async (item) => {
     // Quantity not there in ui, so by default item quantity in cart will be saved as 1 
@@ -38,9 +71,9 @@ export const Shop = () => {
       },
       body: JSON.stringify(product_details)
     }).then((data) => data.json())
-    
+
     console.log(res);
-    
+
     if (res.loc) {
       alert("User Not signed in")
       location.href = res.loc
@@ -51,20 +84,69 @@ export const Shop = () => {
 
   return (
     <section className="overflow-hidden">
-        {
+      {/*Search bar*/}
+      <div className="relative">
+        <label htmlFor="Search" className="sr-only">
+          {" "}
+          Search for...{" "}
+        </label>
+
+        <input
+          type="text"
+          id="Search"
+          placeholder="Jumpsuits.."
+          className="w-full p-3 rounded-md border-2  border-gray-200 py-2.5 pe-10 shadow-sm  dark:bg-gray-800 dark:text-white sm:text-sm"
+          onChange={handleSearch}
+          list="datalistOptions"
+          onKeyDown={fetchItems}
+        />
+        <datalist id="datalistOptions">
+          {
+            autocomplete.map((item) => {
+              return <option key={item.ItemName} value={item.ItemName.toLowerCase()} />
+            })
+          }
+        </datalist>
+
+        <span className="absolute inset-y-0 end-0 grid w-10 place-content-center">
+          <button
+            type="button"
+            className="text-gray-600 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
+          >
+            <span className="sr-only">Search</span>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="h-4 w-4"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+              />
+            </svg>
+          </button>
+        </span>
+      </div>
+      {
         clothes.slice(prev, next).map(cloth => {
-          return(
-      <div key={cloth._id} className="mx-auto max-w-5xl px-5 py-24">
-        <div className="mx-auto flex flex-wrap items-center lg:w-4/5">
-          <img
-            alt="Nike Air Max 21A"
-            className="h-64 w-full rounded object-cover lg:h-96 lg:w-1/2"
-            src={"data:image/png;base64," + cloth.Image}
-          />
-          <div className="mt-6 w-full lg:mt-0 lg:w-1/2 lg:pl-10">
-            <h2 className="text-sm font-semibold tracking-widest text-gray-500">{cloth.Brand}</h2>
-            <h1 className="my-4 text-3xl font-semibold text-gray-200">{cloth.ItemName}</h1>
-            {/* <div className="my-4 flex items-center">
+          // console.log(cloth.Image);
+          return (
+            <div key={cloth._id} className="mx-auto max-w-5xl px-5 py-24">
+              <div className="mx-auto flex flex-wrap items-center lg:w-4/5">
+                <img
+                  alt={cloth.ItemName}
+                  className="h-64 w-full rounded object-cover lg:h-96 lg:w-1/2"
+                  src={"data:image/png;base64," + cloth.Image.toString('base64')}
+                />
+                <div className="mt-6 w-full lg:mt-0 lg:w-1/2 lg:pl-10">
+                  <h2 className="text-sm font-semibold tracking-widest text-gray-500">{cloth.Brand}</h2>
+                  <h1 className="my-4 text-3xl font-semibold text-gray-200">{cloth.ItemName}</h1>
+                  {/* <div className="my-4 flex items-center">
               <span className="flex items-center space-x-1">
                 {[...Array(5)].map((_, i) => (
                   <Star key={i} size={16} className="text-yellow-500" />
@@ -72,49 +154,49 @@ export const Shop = () => {
                 <span className="ml-3 inline-block text-xs text-gray-300 font-semibold">4 Reviews</span>
               </span>
             </div> */}
-            <div className="flex items-center">
-                <span className="mr-3 text-gray-300 text-sm font-semibold">Materials used: </span>
-                <p className="text-gray-400">{cloth.MaterialsUsed}</p>
-            </div>
-            <div>
-              <p className="leading-relaxed text-gray-400">
-              {cloth.ItemDesc}
-              </p>
-            </div>
-            <div className="mb-5 mt-6 flex items-center border-b-2 border-gray-100 pb-5">
-              <div className="flex items-center">
-                <span className="mr-3 text-gray-300 text-sm font-semibold">Occasion: </span>
-                <p className="text-gray-400">{cloth.Occasion}</p>
-              </div>
-              <div className="ml-auto flex items-center">
-                <span className="mr-3 text-sm text-gray-300 font-semibold">Size</span>
-                <div className="relative">
-                  <select className="appearance-none rounded border border-gray-300 py-2 pl-3 pr-10 text-sm focus:border-black focus:outline-none focus:ring-2 focus:ring-black">
-                    <option>S</option>
-                    <option>M</option>
-                    <option>L</option>
-                  </select>
-                  <span className="pointer-events-none absolute right-0 top-0 flex h-full w-10 items-center justify-center text-center text-gray-600">
-                    <ChevronDown size={16} />
-                  </span>
+                  <div className="flex items-center">
+                    <span className="mr-3 text-gray-300 text-sm font-semibold">Materials used: </span>
+                    <p className="text-gray-400">{cloth.MaterialsUsed}</p>
+                  </div>
+                  <div>
+                    <p className="leading-relaxed text-gray-400">
+                      {cloth.ItemDesc}
+                    </p>
+                  </div>
+                  <div className="mb-5 mt-6 flex items-center border-b-2 border-gray-100 pb-5">
+                    <div className="flex items-center">
+                      <span className="mr-3 text-gray-300 text-sm font-semibold">Occasion: </span>
+                      <p className="text-gray-400">{cloth.Occasion}</p>
+                    </div>
+                    <div className="ml-auto flex items-center">
+                      <span className="mr-3 text-sm text-gray-300 font-semibold">Size</span>
+                      <div className="relative">
+                        <select className="appearance-none rounded border border-gray-300 py-2 pl-3 pr-10 text-sm focus:border-black focus:outline-none focus:ring-2 focus:ring-black">
+                          <option>S</option>
+                          <option>M</option>
+                          <option>L</option>
+                        </select>
+                        <span className="pointer-events-none absolute right-0 top-0 flex h-full w-10 items-center justify-center text-center text-gray-600">
+                          <ChevronDown size={16} />
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <span className="title-font text-xl font-bold text-gray-200">{'₹' + cloth.Rate}</span>
+                    <button
+                      type="button"
+                      className="rounded-md bg-yellow-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                      onClick={() => addToCart(cloth)}
+                    >
+                      Add to Cart
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="flex items-center justify-between">
-              <span className="title-font text-xl font-bold text-gray-200">{'₹'+cloth.Rate}</span>
-              <button
-                type="button"
-                className="rounded-md bg-yellow-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-                onClick={() => addToCart(cloth)}
-              >
-                Add to Cart
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
           )
-       })
+        })
       }
 
     </section>
